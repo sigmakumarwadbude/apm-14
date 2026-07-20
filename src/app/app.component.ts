cb314adc421126777c130ee809b48837cb69f55a
Sigma Kumar Wadbude <sigmawadbude@gmail.com>
2026-07-21 01:45:45 +0530

    feat: integrate Bootstrap 4 and Font Awesome 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'sw-root',
   template: `
-  <h1>{{ title }}</h1>
+    <div class="container mt-5">
+      <div class="alert alert-success">Bootstrap 4 Installed Successfully!</div>
 
-<p>Welcome to Angular!</p>
+      <h1>{{ title }}</h1>
+
+      <p>Welcome to Angular!</p>
+
+      <button class="btn btn-primary">
+        <i class="fa fa-home"></i>Click Me
+      </button>
+    </div>
   `,
 })
 export class AppComponent {
